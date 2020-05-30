7898afae61df06e64bf2ee9752230e24969c9076
loliconer <vuedev@163.com>
2020-05-30 11:06:44 +0800

    fix: add access token to request header.

--- server/lib/guard.js
+++ server/lib/guard.js
@@ -1,5 +1,5 @@
 export function auth(req, res, next) {
   const token = req.headers['x-access-token']
-  if (token !== process.env.luowangAccessToken) return res.json({ code: 114, msg: 1 })
+  if (token !== process.env.ACCESS_TOKEN) return res.json({ code: 114, msg: 1 })
   next()
 }
--- src/js/base.js
+++ src/js/base.js
@@ -2,4 +2,5 @@ import '../less/community.less'
 import '../less/layout.less'
 import {$fetch} from '@lovue/utils'
 
+$fetch.setHeader('x-access-token', localStorage.accessToken || '')
 window.$fetch = $fetch
