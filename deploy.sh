eabdcaca2a8a77dd627618db3ae2bdd771088142
loliconer <vuedev@163.com>
2020-05-30 11:20:57 +0800

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-rsync -avz -e "ssh -p 6666" --delete --exclude-from 'deploy_exclude.txt' package.json package-lock.json server scott@112.74.83.47:~/luowang
+rsync -avz -e "ssh -p 6666" --delete --exclude-from 'deploy_exclude.txt' package.json pnpm-lock.yaml server scott@112.74.83.47:~/luowang
 rsync -avz -e "ssh -p 6666" --delete --exclude-from 'deploy_exclude.txt' dist scott@112.74.83.47:~/luowang
